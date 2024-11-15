b339158147cc74cb8d5dafb9856e1d5f291e61fe
ArpanSankesh <arpansankesh830@gmail.com>
2024-11-16 02:08:24 +0530

    Canvas Components

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,6 @@ const App = () => {
     <>
     <div className="w-full h-screen bg-zinc-900 text-white flex flex-wrap">
       <CanvasComponet startIndex={0}/>
-      <CanvasComponet startIndex={150} />
-      <CanvasComponet startIndex={300} />
-      <CanvasComponet startIndex={450} />
-      <CanvasComponet startIndex={600} />
-      <CanvasComponet startIndex={750} />
-      <CanvasComponet startIndex={900} />
-      <CanvasComponet startIndex={1050} />
     </div>
     </>
   )
