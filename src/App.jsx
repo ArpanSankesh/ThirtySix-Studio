5c70a13410a115e722f6c80bcf45224fa6478d00
ArpanSankesh <arpansankesh830@gmail.com>
2024-11-16 02:08:41 +0530

    Canvas Components

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import CanvasComponet from "./components/CanvasComponet"
 const App = () => {
   return (
     <>
-    <div className="w-full h-screen bg-zinc-900 text-white flex flex-wrap">
+    <div className="w-full h-screen flex flex-wrap">
       <CanvasComponet startIndex={0}/>
     </div>
     </>
