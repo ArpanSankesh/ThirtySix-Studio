364bc1240c86bda5b85fc0f83acc17dd24040e79
ArpanSankesh <arpansankesh830@gmail.com>
2024-11-16 02:39:29 +0530

    Canvas Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import CanvasComponet from "./components/CanvasComponet"
+import CanvasComponet from "./CanvasComponet";
+import Data from "./Data";
 
 const App = () => {
   return (
     <>
-    <div className="w-full h-screen flex flex-wrap">
-      <CanvasComponet startIndex={0}/>
-    </div>
+      <div className="w-full bg-black h-screen flex flex-wrap">
+        {Data.map((item, index) => {
+          <div key={index}>
+            {Data.map((canvasDets, index) => {
+              <CanvasComponet key={index} details={canvasDets} />;
+            })}
+          </div>;
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/CanvasComponet.jsx
+++ src/CanvasComponet.jsx
@@ -1,18 +1,20 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useRef, useState } from "react"
-import CanvasImage from "../CanvasImages";
+import CanvasImage from "./CanvasImages";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 
-const CanvasComponet = ({startIndex}) => {
-
+const CanvasComponet = ({details}) => {
+  
+  const {startIndex, numImages, duration, size, top , left , zIndex} = details
   const [index, setIndex] = useState({ value : startIndex})
+  const canvasRef = useRef(null)
   
   useGSAP(() => {
     gsap.to(index,{
-      value: startIndex + 149,
-      duration:3,
+      value: startIndex + numImages - 1 ,
+      duration:duration,
       ease:"linear",
       repeat:-1,
       onUpdate: () =>{
@@ -20,20 +22,28 @@ const CanvasComponet = ({startIndex}) => {
       }
     })
   })
-  const canvasRef = useRef(null)
   useEffect(() => {
     const canvas = canvasRef.current;
     const cxt = canvas.getContext("2d")
     const img = new Image()
     img.src = CanvasImage[index.value]
+    console.log('Image source:', img.src);
     img.onload = () => {
       canvas.width = img.width;
       canvas.height = img.height;
       cxt.drawImage(img,0,0)
+
     }
   }, [index] )
   
-  return <canvas ref={canvasRef} id="canvas" className="w-[18rem] h-[18rem]"></canvas>
+  return <canvas
+  ref={canvasRef}
+  id="canvas"
+  style={{
+    width: `${size}px`,
+    height: `${size}px`
+  }}
+></canvas>
 }
 
 export default CanvasComponet
