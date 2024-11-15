74c4ed9749d220e40984fab7a8cd79cdb43d7ac2
ArpanSankesh <arpansankesh830@gmail.com>
2024-11-16 00:50:16 +0530

    Canvas Components

--- src/components/CanvasComponet.jsx
+++ src/components/CanvasComponet.jsx
@@ -1,16 +1,23 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import CanvasImage from "../CanvasImages";
 
 
 
 const CanvasComponet = () => {
-  
+  const canvasRef = useRef(null)
   useEffect(() => {
-    console.log(CanvasImage);
-    
+    const canvas = canvasRef.current;
+    const cxt = canvas.getContext("2d")
+    const img = new Image()
+    img.src = CanvasImage[0]
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      cxt.drawImage(img,0,0)
+    }
   }, )
   
-  return <canvas id="canvas"></canvas>
+  return <canvas ref={canvasRef} id="canvas" className="w-[18rem] h-[18rem]"></canvas>
 }
 
 export default CanvasComponet
